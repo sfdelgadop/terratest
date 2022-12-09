ab815b1ea59547845245bed44f7c3a56144a856d
Sebastián Delgado <sfdelgadop@unal.edu.co>
2022-12-09 14:27:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
 module tf_azure_appservice_plan_mrasptst {
   source                     = "git@github.com:procter-gamble/terraform-azure-app-service-plan.git?ref=v3.0.0"
   sku_workers                = 1
